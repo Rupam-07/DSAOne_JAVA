57db612fead7a84a24422752f6675c44bfe64aff
Rupam-07 <rupamkarmakarcr7@gmail.com>
2022-09-07 18:12:56 +0530

    Updated

--- Sorting Algorithms/BubbleSort.java
+++ Sorting Algorithms/BubbleSort.java
@@ -17,13 +17,19 @@ public class BubbleSort {
     public static void bubbleSort(int arr[], int n) {
 
         for (int i = 0; i < n - 1; i++) {
+            boolean swap=false;
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
+                    swap=true;
                     // swap using a temp
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
+            }
+            if(!swap){
+               
+                break;
             }
         }
 
