255de0fc2924a5d20a5b733a3185a606b0f566cc
Rupam-07 <rupamkarmakarcr7@gmail.com>
2022-09-07 18:38:44 +0530

    InsertionSort added

---
+++ Sorting Algorithms/InsertionSort.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            insertionSort(arr, n);
+        }
+
+    }
+
+    public static void insertionSort(int[] arr, int n) {
+        for (int i = 1; i < n; i++) {
+            int temp=arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j]>temp){
+                arr[j+1]=arr[j];
+                j--;
+
+            }
+            arr[j+1]=temp;
+        }
+
+        for(int i:arr)
+            System.out.print(i);
+    }
+}
