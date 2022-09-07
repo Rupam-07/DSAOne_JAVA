b3d6f77c9d2a7f95f8807cd61a9de3b2a5438434
Rupam-07 <rupamkarmakarcr7@gmail.com>
2022-09-07 18:40:39 +0530

    InsertionSort Updated

--- Sorting Algorithms/InsertionSort.java
+++ Sorting Algorithms/InsertionSort.java
@@ -16,17 +16,17 @@ public class InsertionSort {
 
     public static void insertionSort(int[] arr, int n) {
         for (int i = 1; i < n; i++) {
-            int temp=arr[i];
-            int j=i-1;
-            while(j>=0 && arr[j]>temp){
-                arr[j+1]=arr[j];
+            int temp = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
                 j--;
 
             }
-            arr[j+1]=temp;
+            arr[j + 1] = temp;
         }
 
-        for(int i:arr)
+        for (int i : arr)
             System.out.print(i);
     }
 }
